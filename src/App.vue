f9baa13d748030224e8f33f6be5ee5ef1f1b63e3
gudlayv <rotachev.g@gmail.com>
2021-09-07 18:47:42 +0300

    CFA-1 - init project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>Header</header>
+  <main></main>
+  <footer>Footer</footer>
+</template>
+
+<style lang="scss">
+header,
+footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background: #f5f5f5;
+  min-height: 70px;
+}
+</style>
