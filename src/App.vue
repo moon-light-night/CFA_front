435d3fd065489e7e5c5152fd3974882208d9f79c
gudlayv <rotachev.g@gmail.com>
2021-09-08 09:20:01 +0300

    CFA-1 - Added vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,51 @@
 <template>
-  <header>Header</header>
-  <main></main>
+  <header>
+    Header
+    <nav>
+      <router-link :to="{name: 'home'}">Home</router-link>
+      <router-link :to="{name: 'second'}">Second</router-link>
+    </nav>
+  </header>
+  <main>
+    <router-view/>
+  </main>
   <footer>Footer</footer>
 </template>
 
 <style lang="scss">
-header,
+header {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  min-height: 70px;
+
+  background: midnightblue;
+  color: whitesmoke;
+
+  padding: 1rem;
+
+  nav {
+    margin-left: 2rem;
+
+    a {
+      color: whitesmoke;
+      text-decoration: none;
+      transition: color 200ms ease-in-out, border 200ms ease-in-out;
+
+      &:not(:first-child) {
+        margin-left: 1rem;
+      }
+
+      &.router-link-active {
+        font-weight: 700;
+        color: darkorange;
+        border-bottom: currentColor 1px solid;
+      }
+    }
+
+  }
+}
+
 footer {
   display: flex;
   justify-content: center;
---
+++ src/views/Second.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Second</h1>
+</template>
+
+<script>
+export default {
+  name: "Second"
+}
+</script>
+
+<style scoped>
+h1 {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+  min-height: 100%;
+
+  background: darkorchid;
+  color: whitesmoke;
+  font-size: 10vmin;
+}
+</style>
